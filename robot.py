ebe6ee9d77c1b75b38b0c8d28767f35df616b3df
Piotr Blaszyk <piotrblaszyk@Piotr-Baszyk-Macbook-Pro.local>
2024-03-01 18:20:25 +0000

    some progress

--- robot.py
+++ robot.py
@@ -22,10 +22,10 @@ import configuration
 from graphics import PathToDraw
 
 
-ACTION_SIZE = 4
+ACTION_SIZE = 3
 GAMMA = 0.9
-epsilon = 0.9
-alpha = 0.1
+EPSILON = 0.75
+alpha = 1.0
 
 class Robot:
 
@@ -43,16 +43,27 @@ class Robot:
         self.total_rewards = []
         self.total_reward = 0
 
+        self.visit_count = np.zeros((10, 10))
+        self.cell_speed = - np.ones(constants.WORLD_SIZE ** 2)
+        self.cell_angle = - np.ones(constants.WORLD_SIZE ** 2)
+
+        self.our_speed = constants.ROBOT_MAX_ACTION / 2
+        self.our_magnitude = self.our_speed * float(np.sqrt(2))
+
         self.returns = [[[] for j in range(ACTION_SIZE)] for i in range(constants.WORLD_SIZE ** 2)]
 
-        self.num_episodes = 2
-        self.max_steps = 2
+        # self.num_episodes = 2
+        self.max_steps = 400
         self.rng = np.random.default_rng()
 
         self.i = 0
         self.j = 0
 
-        self.next_actions = ['demo']
+        self.next_actions = ['demo', 'demo']
+        self.max_dist = 100 * float(np.sqrt(2))
+
+        self.replay_states = None
+        self.replay_actions = None
 
     def get_next_action_type(self, state, money_remaining):
         if len(self.next_actions) > 0:
@@ -65,6 +76,7 @@ class Robot:
         state_ix = self.quantised_space_to_ix(self.quantise_space(state))
         best_a = np.argmax(self.Q[state_ix, :])
         b_policy = np.zeros(ACTION_SIZE)
+        epsilon = self.calc_epsilon()
         for k in range(ACTION_SIZE):
             if k == best_a:
                 b_policy[k] = 1 - epsilon + epsilon / ACTION_SIZE
@@ -92,13 +104,22 @@ class Robot:
         return unquantised_action
 
     def process_transition(self, state, action, next_state, money_remaining):
-        
-        reward = self.reward(next_state)
+        state = state.reshape((2, 1))
+        next_state = next_state.reshape((2, 1))
+        action = action.reshape((2, 1))
+
+        state_speed = self.dist(state, next_state) / self.our_magnitude
+
         quantised_state = self.quantise_space(state)
         in_goal_state = self.reached_goal(quantised_state)
         state_ix = self.quantised_space_to_ix(quantised_state)
-        next_state_ix = self.quantised_space_to_ix(self.quantise_space(next_state))
+        quantised_next_state = self.quantise_space(next_state)
+        next_state_ix = self.quantised_space_to_ix(quantised_next_state)
         action_ix = self.quantise_action(action)
+        reward = self.reward(next_state, quantised_next_state, next_state_ix)
+        x, y = self.get_bins(next_state)
+        self.visit_count[x, y] += 1
+        self.cell_speed[state_ix] = state_speed
 
         self.total_reward += reward
         self.Q[state_ix, action_ix] = self.Q[state_ix, action_ix] + alpha * (reward + GAMMA * max(self.Q[next_state_ix, :]) - self.Q[state_ix, action_ix])
@@ -114,7 +135,8 @@ class Robot:
             self.total_rewards.append(self.total_reward)
             self.total_reward = 0
 
-            path_to_draw = PathToDraw(path=self.path, colour=[0, 0, 255], width=1)
+            path = np.stack(self.path)
+            path_to_draw = PathToDraw(path=path, colour=[255, 255, 255], width=1)
             self.paths_to_draw.append(path_to_draw)
             self.path = []
 
@@ -131,10 +153,17 @@ class Robot:
             if not self.demo:
                 self.j = 0
                 self.i += 1
+            
+            print(fr'{self.i} episodes done', 'money', money_remaining)
+
+            self.process_demonstration(self.replay_states, self.replay_actions, None)
 
     def process_demonstration(self, demonstration_states, demonstration_actions, money_remaining):
-        # path_to_draw = PathToDraw(path=demonstration_states, colour=[0, 0, 255], width=2)
-        # self.paths_to_draw.append(path_to_draw)
+        self.replay_states = demonstration_states
+        self.replay_actions = demonstration_actions
+
+        path_to_draw = PathToDraw(path=demonstration_states, colour=[0, 0, 255], width=2)
+        self.paths_to_draw.append(path_to_draw)
         n = demonstration_states.shape[0]
         self.demo = True
         for i in range(n - 1):
@@ -144,19 +173,29 @@ class Robot:
             self.process_transition(state, action, next_state, money_remaining)
         self.demo = False
 
-        print('demo has been processed')
+        print('demo has been processed', 'money', money_remaining)
 
     def dynamics_model(self, state, action):
         next_state = state + action
         return next_state
 
-    def reward(self, quantised_state):
+    def reward(self, state, quantised_state, state_ix):
         goal = self.quantise_space(self.goal_state)
         dist_to_goal = self.dist(quantised_state, goal)
-        if dist_to_goal < constants.TEST_DISTANCE_THRESHOLD:
-            return 100
-        else:
-            return -dist_to_goal
+        # dist_term = 5 * (self.max_dist - dist_to_goal) / self.max_dist
+        dist_term = 10 / (dist_to_goal ** 2)
+        res = dist_term
+        # x, y = self.get_bins(state)
+        # visit_term = 1 / (1 + self.visit_count[x, y])
+        # res += visit_term
+        # if self.cell_speed[state_ix] < 0:
+        #     speed_term = 0
+        # else:
+        #     speed_term = self.cell_speed[state_ix]
+        # res += speed_term
+        if dist_to_goal == 0:
+            res += 1
+        return res
 
     def dist(self, x, y):
        return np.linalg.norm(x=x-y, ord=2)
@@ -203,118 +242,27 @@ class Robot:
     def unquantise_action(self, quantised_a):
         actions = [[-1, -1], [1, -1], [1, 1], [-1, 1]]
         action = actions[quantised_a]
-        action = np.array(action, dtype=np.float32)
+        action = self.our_speed * np.array(action, dtype=np.float32)
         return action.reshape((2, 1))
 
     def reached_goal(self, quantised_state):
        goal = self.quantise_space(self.goal_state)
        return (quantised_state == goal).all()
     
+    def calc_epsilon(self):
+        return EPSILON
+        return max(EPSILON - (1 / 4_000) * (self.i * self.max_steps + self.j), 0.333)
+
+    def get_bins(self, state):
+        foo = state.copy()
+        foo = foo.squeeze()
+        k = constants.WORLD_SIZE / self.visit_count.shape[0]
+        foo /= k
+        x = int(foo[0])
+        y = int(foo[1])
+        return x, y
+    
     # def calc_final_policy(self):
     #     for i in range(constants.WORLD_SIZE ** 2):
     #         best_a = np.argmax(self.Q[i, :])
     #         self.final_policy[i, best_a] = 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class TD_agent(object):
-  def solve(self, env):
-    """
-    Solve a given Maze environment using Temporal Difference learning
-    input: env {Maze object} -- Maze to solve
-    output:
-      - policy {np.array} -- Optimal policy found to solve the given Maze environment
-      - values {list of np.array} -- List of successive value functions for each episode
-      - total_rewards {list of float} -- Corresponding list of successive total non-discounted sum of reward for each episode
-    """
-
-    Q = np.random.rand(constants.WORLD_SIZE ** 2, ACTION_SIZE)
-    V = np.zeros(constants.WORLD_SIZE ** 2)
-    policy = np.zeros((constants.WORLD_SIZE ** 2, ACTION_SIZE))
-    values = [V]
-    total_rewards = []
-
-    epsilon = 0.9
-    alpha = 0.1
-    returns = [[[] for j in range(ACTION_SIZE)] for i in range(constants.WORLD_SIZE ** 2)]
-
-    num_episodes = 2_000
-    max_steps = 1_000
-    rng = np.random.default_rng()
-    i = 0
-
-    while i < num_episodes:
-      t, state, reward, done = env.reset()
-      total_reward = reward
-      for j in range(max_steps):
-        best_a = np.argmax(Q[state, :])
-        b_policy = np.zeros(ACTION_SIZE)
-        for k in range(ACTION_SIZE):
-          if k == best_a:
-            b_policy[k] = 1 - epsilon + epsilon / ACTION_SIZE
-          else:
-            b_policy[k] = epsilon / ACTION_SIZE
-
-        r = rng.random()
-        act = None
-        for k in range(ACTION_SIZE):
-          p = b_policy[k]
-          if p >= r:
-            act = k
-            break
-          r -= p
-
-        assert(act is not None)
-        prv_state = state
-        t, state, reward, done = env.step(act)
-        total_reward += reward
-        Q[prv_state, act] = Q[prv_state, act] + alpha * (reward + GAMMA * max(Q[state, :]) - Q[prv_state, act])
-
-        if done:
-          break
-
-      total_rewards.append(total_reward)
-
-      V = np.zeros(constants.WORLD_SIZE ** 2)
-      for j in range(constants.WORLD_SIZE ** 2):
-        best_a = np.argmax(policy[j, :])
-        V[j] = Q[j, best_a]
-
-      values.append(V)
-
-      i += 1
-
-    for i in range(constants.WORLD_SIZE ** 2):
-      best_a = np.argmax(Q[i, :])
-      policy[i, best_a] = 1
-
-    return policy, values, total_rewards
