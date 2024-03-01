d1bb9c92ad30d4c0fd2be815cd45b57a9790e774
Piotr Blaszyk <piotrblaszyk@Piotr-Baszyk-Macbook-Pro.local>
2024-03-01 07:09:04 +0000

    .

--- robot.py
+++ robot.py
@@ -33,34 +33,37 @@ class Robot:
         self.goal_state = goal_state
         self.paths_to_draw = []
 
+        self.path = []
+
         self.Q = np.random.rand(constants.WORLD_SIZE ** 2, ACTION_SIZE)
         self.V = np.zeros(constants.WORLD_SIZE ** 2)
         self.policy = np.zeros((constants.WORLD_SIZE ** 2, ACTION_SIZE))
+        self.final_policy = np.zeros((constants.WORLD_SIZE ** 2, ACTION_SIZE))
         self.values = [self.V]
         self.total_rewards = []
         self.total_reward = 0
 
         self.returns = [[[] for j in range(ACTION_SIZE)] for i in range(constants.WORLD_SIZE ** 2)]
 
-        self.num_episodes = 2_000
-        self.max_steps = 1_000
+        self.num_episodes = 2
+        self.max_steps = 2
         self.rng = np.random.default_rng()
 
         self.i = 0
         self.j = 0
 
-        self.next_action = None
+        self.next_actions = ['demo']
 
     def get_next_action_type(self, state, money_remaining):
-        if self.next_action is not None:
-            res = self.next_action
-            self.next_action = None
+        if len(self.next_actions) > 0:
+            res = self.next_actions.pop()
             return res
         else:
             return 'step'
 
     def get_next_action_training(self, state, money_remaining):
-        best_a = np.argmax(self.Q[state, :])
+        state_ix = self.quantised_space_to_ix(self.quantise_space(state))
+        best_a = np.argmax(self.Q[state_ix, :])
         b_policy = np.zeros(ACTION_SIZE)
         for k in range(ACTION_SIZE):
             if k == best_a:
@@ -78,33 +81,70 @@ class Robot:
             r -= p
 
         assert(act is not None)
-        return act
+        unquantised_action = self.unquantise_action(act)
+        return unquantised_action
 
     def get_next_action_testing(self, state):
-        pass
+        state_ix = self.quantised_space_to_ix(self.quantise_space(state))
+        one_hot_actions = self.final_policy[state_ix, :]
+        quantised_action = int(np.argmax(one_hot_actions))
+        unquantised_action = self.unquantise_action(quantised_action)
+        return unquantised_action
 
     def process_transition(self, state, action, next_state, money_remaining):
         
         reward = self.reward(next_state)
-        in_goal_state = self.reached_goal(self.quantise_space(state))
+        quantised_state = self.quantise_space(state)
+        in_goal_state = self.reached_goal(quantised_state)
+        state_ix = self.quantised_space_to_ix(quantised_state)
+        next_state_ix = self.quantised_space_to_ix(self.quantise_space(next_state))
+        action_ix = self.quantise_action(action)
 
         self.total_reward += reward
-        self.Q[state, action] = self.Q[state, action] + alpha * (reward + GAMMA * max(self.Q[next_state, :]) - self.Q[state, action])
+        self.Q[state_ix, action_ix] = self.Q[state_ix, action_ix] + alpha * (reward + GAMMA * max(self.Q[next_state_ix, :]) - self.Q[state_ix, action_ix])
+
+        self.path.append(state)
+
+        if not self.demo:
+            self.j += 1
 
         if in_goal_state or self.j == self.max_steps:
-            self.next_action = 'reset'
+            self.next_actions.append('reset')
+
             self.total_rewards.append(self.total_reward)
             self.total_reward = 0
+
+            path_to_draw = PathToDraw(path=self.path, colour=[0, 0, 255], width=1)
+            self.paths_to_draw.append(path_to_draw)
+            self.path = []
+
             V = np.zeros(constants.WORLD_SIZE ** 2)
             for k in range(constants.WORLD_SIZE ** 2):
                 best_a = np.argmax(self.policy[k, :])
                 V[k] = self.Q[k, best_a]
 
+                best_a = np.argmax(self.Q[k, :])
+                self.final_policy[k, best_a] = 1
+
             self.values.append(V)
-            self.i += 1
+
+            if not self.demo:
+                self.j = 0
+                self.i += 1
 
     def process_demonstration(self, demonstration_states, demonstration_actions, money_remaining):
-       pass
+        # path_to_draw = PathToDraw(path=demonstration_states, colour=[0, 0, 255], width=2)
+        # self.paths_to_draw.append(path_to_draw)
+        n = demonstration_states.shape[0]
+        self.demo = True
+        for i in range(n - 1):
+            state = demonstration_states[i]
+            action = demonstration_actions[i]
+            next_state = demonstration_states[i + 1]
+            self.process_transition(state, action, next_state, money_remaining)
+        self.demo = False
+
+        print('demo has been processed')
 
     def dynamics_model(self, state, action):
         next_state = state + action
@@ -126,6 +166,16 @@ class Robot:
         cell_y = int(state[1])
         return np.array([cell_x, cell_y]).reshape((2, 1))
     
+    def quantised_space_to_ix(self, state):
+        cell_x = int(state[0])
+        cell_y = int(state[1])
+        return constants.WORLD_SIZE * cell_x + cell_y
+
+    def ix_to_quantised_space(self, n):
+        cell_x = n // constants.WORLD_SIZE
+        cell_y = n % constants.WORLD_SIZE
+        return np.array([cell_x, cell_y]).reshape((2, 1))
+    
     def unquantise_space(self, state):
         return state + 0.5
     
@@ -142,11 +192,12 @@ class Robot:
         # 0 <= a < 2 * np.pi
         a /= np.pi
         # 0 <= a < 2
-        a *= 2
-        # 0 <= a < 4
+        a /= 2
+        # 0 <= a < 1
+        a *= ACTION_SIZE
         res = int(a)
-        if res == 4:
-            res = 3
+        if res == ACTION_SIZE:
+            res = ACTION_SIZE - 1
         return res
 
     def unquantise_action(self, quantised_a):
@@ -157,12 +208,12 @@ class Robot:
 
     def reached_goal(self, quantised_state):
        goal = self.quantise_space(self.goal_state)
-       return quantised_state == goal
+       return (quantised_state == goal).all()
     
-    def calc_policy(self):
-        for i in range(constants.WORLD_SIZE ** 2):
-            best_a = np.argmax(self.Q[i, :])
-            self.policy[i, best_a] = 1
+    # def calc_final_policy(self):
+    #     for i in range(constants.WORLD_SIZE ** 2):
+    #         best_a = np.argmax(self.Q[i, :])
+    #         self.final_policy[i, best_a] = 1
 
 
 
